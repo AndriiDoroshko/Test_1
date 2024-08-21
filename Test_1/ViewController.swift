4ebbf990f19e37412c23c0d972717018d27ae3a2
Andrii Doroshko <Andr.dorosh4@gmail.com>
2024-08-20 21:31:56 -0400

    Change to red according to PR comments

--- Test_1/ViewController.swift
+++ Test_1/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.view.backgroundColor = UIColor.blue
+        super.view.backgroundColor = UIColor.red
         // Do any additional setup after loading the view.
     }
 
