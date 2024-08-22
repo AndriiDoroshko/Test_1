a0897de640e134439378750f826ee5ad99f2cb68
Andrii Doroshko <Andr.dorosh4@gmail.com>
2024-08-21 21:00:24 -0400

    Add Alamofire and Realm to project

--- Test_1/ViewController.swift
+++ Test_1/ViewController.swift
@@ -6,15 +6,33 @@
 //
 
 import UIKit
+import Alamofire
+import Realm
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         super.view.backgroundColor = UIColor.orange
+        getData()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func getData() {
+        AF.request(
+            "https://api.weatherapi.com/v1/current.json?q=New%20York&key=1ad656828c264d949c5170050242108")
+        .response { response in
+            
+        }
+    }
+    
+    func saveData() {
+        let realm = try! Realm()
+        
+        try! realm.write({
+//            realm.add("Response")
+        })
+    }
 }
 
